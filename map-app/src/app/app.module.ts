4c38a4d38e66cfff9bc32fb3a684b2d8b8bbe858
Casprovy <casprovy.tgs@gmail.com>
2019-01-18 18:56:04 +0100

    navbar, container created

--- map-app/src/app/app.module.ts
+++ map-app/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ContainerComponent } from './container/container.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ContainerComponent
   ],
   imports: [
     BrowserModule,
