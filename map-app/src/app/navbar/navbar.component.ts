0a86b00e7359dcc5b998553c8ff737011678d59a
Pancho <pinchepanchopincho@gmail.com>
2019-01-19 11:20:37 +0100

    FB Login First Stage

--- map-app/src/app/navbar/navbar.component.ts
+++ map-app/src/app/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+declare var window: any;
+declare var FB: any;
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +9,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
+
   constructor() { }
 
   ngOnInit() {
+    (window as any).fbAsyncInit = function() {
+      FB.init({
+        appId      : '2543129815913697',
+        cookie     : true,
+        xfbml      : true,
+        version    : 'v3.1'
+      });
+      FB.AppEvents.logPageView();
+    };
+
+    (function(d, s, id){
+       var js, fjs = d.getElementsByTagName(s)[0];
+       if (d.getElementById(id)) {return;}
+       js = d.createElement(s); js.id = id;
+       js.src = "https://connect.facebook.net/en_US/sdk.js";
+       fjs.parentNode.insertBefore(js, fjs);
+     }(document, 'script', 'facebook-jssdk'));
+  }
+
+  submitLogin(){
+    console.log("submit login to facebook");
+    // FB.login();
+    FB.login((response)=>
+        {
+          console.log('submitLogin',response);
+          if (response.authResponse)
+          {
+            console.log('success')
+            //login success
+            //login success code here
+            //redirect to home page
+           }
+           else
+           {
+           console.log('User login failed');
+         }
+      });
+
   }
 
 }
