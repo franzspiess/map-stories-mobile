2a561377e72346591f99cc0ecf50b29b3018332b
Casprovy <casprovy.tgs@gmail.com>
2019-01-21 19:10:00 +0100

    photo works

--- map-app/src/app/event/event.component.ts
+++ map-app/src/app/event/event.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Event } from '../Models'
 
+import * as AWS from 'aws-sdk/global';
+import * as S3 from 'aws-sdk/clients/s3';
+
+
 @Component({
   selector: 'app-event',
   templateUrl: './event.component.html',
@@ -12,26 +16,54 @@ export class EventComponent implements OnInit {
 
   newEvent = new Event ();
 
-  title = 'app';
-  selectedFile = "";
+  public imagePath;
+  iosPhoto: File;
+  imageUrl: any;
+  
 
   onFileSelected(event)
   {
-    this.selectedFile = event.target.files[0];
-    console.log(this.selectedFile)
-    
+    this.iosPhoto = event.target.files[0];
+    const reader = new FileReader();
+    this.imagePath = event.target.files;
+    reader.readAsDataURL(this.iosPhoto);
+    reader.onload = (_event) => {this.imageUrl = reader.result;}
   }
 
+  
   onUpload()
   {
-    console.log(this.selectedFile); // You can use FormData upload to backend server
+    const s3 = new S3(
+      {
+        accessKeyId: 'AKIAIGN6OJQFPUNLC4VA',
+        secretAccessKey: '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',
+        region: 'eu-west-2'
+      }
+    );
+ 
+    const params = {
+      Bucket: 'legacy-cw',
+      Key: '/' + this.iosPhoto.name,
+      Body: this.iosPhoto,
+      // ACL: 'public-read',
+    };
+ 
+    s3.upload(params, function (err, data) {
+      if (err) {
+        console.log('There was an error uploading your file: ', err);
+        return false;
+      }
+ 
+      console.log('Successfully uploaded file.', data);
+      return true;
+    });
+
+    console.log(this.iosPhoto);
+  
   }
-
-
 
   ngOnInit() {
     console.log(this.newEvent);
-
   }
 
   get diagnostic() { return JSON.stringify(this.newEvent); }
